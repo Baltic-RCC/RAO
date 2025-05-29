647e591698493513c9242b09a56bc12246392617
martynas.karobcikas <mkarobcikas@gmail.com>
2025-05-29 16:43:12 +0300

    bugfix with input retriever config parsing

--- input_retriever/worker.py
+++ input_retriever/worker.py
@@ -2,8 +2,9 @@ from integrations import rmq
 from input_retriever.handlers import HandlerMetadataToObjectStorage
 from common.config_parser import parse_app_properties
 from loguru import logger
+from pathlib import Path
 
-parse_app_properties(caller_globals=globals(), path="../input_retriever/config.properties")
+parse_app_properties(caller_globals=globals(), path=str(Path(__file__).parent.joinpath("config.properties")))
 
 # RabbitMQ consumer implementation
 consumer = rmq.RMQConsumer(
